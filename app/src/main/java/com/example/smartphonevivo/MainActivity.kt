6054709fcf767e29208bb37d71c92706a759d1b9
JIacmuk <vasilev.sana@bk.ru>
2024-06-25 14:18:38 +0300

    test1

--- app/src/main/java/com/example/smartphonevivo/MainActivity.kt
+++ app/src/main/java/com/example/smartphonevivo/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.smartphonevivo.fragments.MainFragment
 
 class MainActivity : AppCompatActivity() {
-    //тест
+    //тест1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
